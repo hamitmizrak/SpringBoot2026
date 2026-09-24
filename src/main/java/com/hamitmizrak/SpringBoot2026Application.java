3dd3a48a7f9b8639cf6531a8c78a46b1d48db60f
Hamit Mızrak <hamitmizrak@gmail.com>
2026-09-24 20:27:59 +0300

    pom.xml

--- src/main/java/com/hamitmizrak/SpringBoot2026Application.java
+++ src/main/java/com/hamitmizrak/SpringBoot2026Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBoot2026Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2026Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2026Application.class, args);
+    }
 
 }
